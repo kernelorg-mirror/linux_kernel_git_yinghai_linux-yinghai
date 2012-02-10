fee9a731d7bc9b07d6bbc3910342687f9464e207
Yinghai Lu <yinghai@kernel.org>
2012-02-10 15:23:56 -0800

    PCI: print out suggestion about using pci=realloc
    let user know they could try if pci=realloc could help.
    
    -v2: update suggestion text.
    
    Suggested-by: Jesse Barnes <jbarnes@virtuousgeek.org>
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -1346,6 +1346,9 @@ again:
 		goto enable_and_dump;
 
 	if (tried_times >= pci_try_num) {
+		if (pci_realloc_enable == enable_not_set)
+			printk(KERN_INFO "Some PCI device resources are unassigned, try booting with pci=realloc\n");
+
 		free_list(&fail_head);
 		goto enable_and_dump;
 	}
