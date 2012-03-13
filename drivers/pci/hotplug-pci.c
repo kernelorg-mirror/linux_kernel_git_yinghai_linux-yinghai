a14b064fdecbb6ebcc4a169452ba88794a43e0a7
Yinghai Lu <yinghai@kernel.org>
2012-03-13 00:15:03 -0700

    PCI, pciehp: Separate pci_hp_add_bridge()
    Will use it for other pci hp hotplug support
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/hotplug-pci.c
+++ drivers/pci/hotplug-pci.c
@@ -4,6 +4,19 @@
 #include <linux/export.h>
 #include "pci.h"
 
+int __ref pci_hp_add_bridge(struct pci_dev *dev)
+{
+	struct pci_bus *parent = dev->bus;
+	int pass, busnr = parent->secondary;
+
+	for (pass = 0; pass < 2; pass++)
+		busnr = pci_scan_bridge(parent, dev, busnr, pass);
+	if (!dev->subordinate)
+		return -1;
+
+	return 0;
+}
+EXPORT_SYMBOL(pci_hp_add_bridge);
 
 unsigned int __devinit pci_do_scan_bus(struct pci_bus *bus)
 {
