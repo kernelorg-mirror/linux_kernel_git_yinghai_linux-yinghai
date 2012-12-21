0450313ec367506276cb5fd65d508d1a0acc655a
Yinghai Lu <yinghai@kernel.org>
2012-12-20 19:53:45 -0800

    PCI, libata: remove find_bridge in acpi_bus_type
    The struct member is not used anymore, remove it.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Jeff Garzik <jgarzik@pobox.com>
    Cc: linux-ide@vger.kernel.org
    Acked-by: Jeff Garzik <jgarzik@redhat.com>

--- drivers/ata/libata-acpi.c
+++ drivers/ata/libata-acpi.c
@@ -1177,13 +1177,7 @@ static int ata_acpi_find_device(struct device *dev, acpi_handle *handle)
 		return -ENODEV;
 }
 
-static int ata_acpi_find_dummy(struct device *dev, acpi_handle *handle)
-{
-	return -ENODEV;
-}
-
 static struct acpi_bus_type ata_acpi_bus = {
-	.find_bridge = ata_acpi_find_dummy,
 	.find_device = ata_acpi_find_device,
 };
 
