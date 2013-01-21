8fe4ad15a84af7e25c97c5ec6cd1349129a00fe4
Yinghai Lu <yinghai@kernel.org>
2013-01-21 13:04:29 -0800

    PCI: Use for_each_pci_dev_resource pci_reassigndev
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -3785,8 +3785,7 @@ void pci_reassigndev_resource_alignment(struct pci_dev *dev)
 	pci_write_config_word(dev, PCI_COMMAND, command);
 
 	align = pci_specified_resource_alignment(dev);
-	for (i = 0; i < PCI_BRIDGE_RESOURCES; i++) {
-		r = &dev->resource[i];
+	for_each_pci_resource(dev, r, i, PCI_NOBRIDGE_RES) {
 		if (!(r->flags & IORESOURCE_MEM))
 			continue;
 		size = resource_size(r);
@@ -3805,8 +3804,7 @@ void pci_reassigndev_resource_alignment(struct pci_dev *dev)
 	 */
 	if (dev->hdr_type == PCI_HEADER_TYPE_BRIDGE &&
 	    (dev->class >> 8) == PCI_CLASS_BRIDGE_PCI) {
-		for (i = PCI_BRIDGE_RESOURCES; i < PCI_NUM_RESOURCES; i++) {
-			r = &dev->resource[i];
+		for_each_pci_resource(dev, r, i, PCI_BRIDGE_RES) {
 			if (!(r->flags & IORESOURCE_MEM))
 				continue;
 			r->end = resource_size(r) - 1;
