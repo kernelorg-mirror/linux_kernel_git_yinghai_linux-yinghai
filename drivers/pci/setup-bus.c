6a466bcce60279739061b9dbbd189761254cee3c
Yinghai Lu <yinghai@kernel.org>
2013-07-22 14:12:45 -0700

    PCI: Don't use temp bus for pci_bus_release_bridge_resources
    as later bus can not be used as temp variable after we change to
    per root bus handling with assign unassigned resources.
    
    Per Bjorn, separate it from big patch that handing assign_unssigned per root bus.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -1458,12 +1458,11 @@ again:
 	 * Try to release leaf bridge's resources that doesn't fit resource of
 	 * child device under that bridge
 	 */
-	list_for_each_entry(fail_res, &fail_head, list) {
-		bus = fail_res->dev->bus;
-		pci_bus_release_bridge_resources(bus,
+	list_for_each_entry(fail_res, &fail_head, list)
+		pci_bus_release_bridge_resources(fail_res->dev->bus,
 						 fail_res->flags & type_mask,
 						 rel_type);
-	}
+
 	/* restore size and flags */
 	list_for_each_entry(fail_res, &fail_head, list) {
 		struct resource *res = fail_res->res;
