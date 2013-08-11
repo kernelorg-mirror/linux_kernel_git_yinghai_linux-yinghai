e21e2b16073c6f495dc0e605b01a1d0a48aacb31
Yinghai Lu <yinghai@kernel.org>
2013-08-10 19:31:03 -0700

    PCI, x86, ACPI: Link acpi ioapic register to ioapic
    During ioapic hotplug, acpi_register_ioapic will be called.
    Now for x86, that function is blank.
    Fill that will update __mp_register_ioapic to use those ioapic.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- arch/x86/kernel/acpi/boot.c
+++ arch/x86/kernel/acpi/boot.c
@@ -707,16 +707,18 @@ EXPORT_SYMBOL(acpi_unmap_lsapic);
 
 int acpi_register_ioapic(acpi_handle handle, u64 phys_addr, u32 gsi_base)
 {
-	/* TBD */
-	return -EINVAL;
+	unsigned long long id = 0;
+
+	acpi_evaluate_integer(handle, "_UID", NULL, &id);
+
+	return	__mp_register_ioapic(id, phys_addr, gsi_base, true);
 }
 
 EXPORT_SYMBOL(acpi_register_ioapic);
 
 int acpi_unregister_ioapic(acpi_handle handle, u32 gsi_base)
 {
-	/* TBD */
-	return -EINVAL;
+	return mp_unregister_ioapic(gsi_base);
 }
 
 EXPORT_SYMBOL(acpi_unregister_ioapic);
