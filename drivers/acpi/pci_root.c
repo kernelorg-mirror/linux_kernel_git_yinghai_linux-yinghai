c1304c376c75abffac6ff6c67914d8f57ff05d9e
Yinghai Lu <yinghai@kernel.org>
2013-08-10 19:31:03 -0700

    PCI, x86, ACPI: Enable ioapic hotplug support with acpi host bridge.
    We need to have ioapic setup before normal pci drivers.
    otherwise other pci driver can not setup irq.
    
    So we should not treat them as normal pci devices.
    Also we will need to support ioapic hotplug without pci device around.
    
    We need to call ioapic add/remove during host-bridge add/remove.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/acpi/pci_root.c
+++ drivers/acpi/pci_root.c
@@ -532,6 +532,8 @@ static int acpi_pci_root_add(struct acpi_device *device,
 		pci_enable_bridges(root->bus);
 	}
 
+	acpi_pci_ioapic_add(root);
+
 	pci_bus_add_devices(root->bus);
 	return 1;
 
@@ -545,6 +547,8 @@ static void acpi_pci_root_remove(struct acpi_device *device)
 	struct acpi_pci_root *root = acpi_driver_data(device);
 
 	pci_stop_root_bus(root->bus);
+
+	acpi_pci_ioapic_remove(root);
 
 	device_set_run_wake(root->bus->bridge, false);
 	pci_acpi_remove_bus_pm_notifier(device);
