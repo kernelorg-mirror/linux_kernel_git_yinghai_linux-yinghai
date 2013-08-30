d34bdccee5af5563db8c5648d15a944b6bfecf59
Yinghai Lu <yinghai@kernel.org>
2013-08-30 13:56:03 -0700

    PCI: Stop sriov after stop PF if PF's driver skip that
    After commit dc087f2f6a2925e81831f3016b9cbb6e470e7423
    (PCI: Simplify IOV implementation and fix reference count races)
    VF need to be removed via virtfn_remove to make sure ref to PF
    is put back.
    
    Some driver (like ixgbe) does not call pci_disable_sriov() if
    sriov is enabled via /sys/.../sriov_numvfs setting.
    ixgbe does allow driver for PF get detached, but still have VFs
    around.
    
    But how about PF get removed via /sys or pciehp finally?
    
    During hot-remove, VF will still hold one ref to PF and it
    prevent PF to be removed.
    That make the next hot-add fails, as old PF dev struct is still around.
    
    We need to add pci_disable_sriov() calling during stop PF .
    
    Need this one for v3.11
    
    -v2: Accoring to Bjorn, move that calling to pci_stop_dev.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Jiang Liu <liuj97@gmail.com>
    Cc: Alexander Duyck <alexander.h.duyck@intel.com>
    Cc: Donald Dutile <ddutile@redhat.com>
    Cc: Greg Rose <gregory.v.rose@intel.com>

--- drivers/pci/remove.c
+++ drivers/pci/remove.c
@@ -25,6 +25,8 @@ static void pci_stop_dev(struct pci_dev *dev)
 		pci_proc_detach_device(dev);
 		pci_remove_sysfs_dev_files(dev);
 		device_del(&dev->dev);
+		/* remove VF, if PF driver skip that */
+		pci_disable_sriov(dev);
 		dev->is_added = 0;
 	}
 
