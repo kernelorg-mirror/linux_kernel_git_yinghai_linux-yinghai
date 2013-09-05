fb8c4c48acb27452fa1ee657d8f5a54ccdc1ee9a
Yinghai Lu <yinghai@kernel.org>
2013-09-04 23:11:09 -0700

    x86, mm: Parse numa info early
    Parsing numa info has been separated to two functions now.
    
    early_initmem_info() only parse info in numa_meminfo and
    nodes_parsed. still keep numaq, acpi_numa, amd_numa, dummy
    fall back sequence working.
    
    SLIT and numa emulation handling are still left in initmem_init().
    
    Call early_initmem_init before init_mem_mapping() to prepare
    to use numa_info with it.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: Jacob Shin <jacob.shin@amd.com>
    Reviewed-by: Tang Chen <tangchen@cn.fujitsu.com>
    Tested-by: Tang Chen <tangchen@cn.fujitsu.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1120,13 +1120,21 @@ void __init setup_arch(char **cmdline_p)
 	trim_platform_memory_ranges();
 	trim_low_memory_range();
 
+	/*
+	 * Parse the ACPI tables for possible boot-time SMP configuration.
+	 */
+	acpi_initrd_override_copy();
+	acpi_boot_table_init();
+	early_acpi_boot_init();
+	early_initmem_init();
 	init_mem_mapping();
-
+	memblock_set_current_limit(get_max_mapped());
 	early_trap_pf_init();
+
+	reserve_initrd();
 
 	setup_real_mode();
 
-	memblock_set_current_limit(get_max_mapped());
 	dma_contiguous_reserve(0);
 
 	/*
@@ -1140,24 +1148,12 @@ void __init setup_arch(char **cmdline_p)
 	/* Allocate bigger log buffer */
 	setup_log_buf(1);
 
-	acpi_initrd_override_copy();
-
-	reserve_initrd();
-
 	reserve_crashkernel();
 
 	vsmp_init();
 
 	io_delay_init();
 
-	/*
-	 * Parse the ACPI tables for possible boot-time SMP configuration.
-	 */
-	acpi_boot_table_init();
-
-	early_acpi_boot_init();
-
-	early_initmem_init();
 	initmem_init();
 	memblock_find_dma_reserve();
 
