efacaa93d5ac42aea5d64d45952a169ca174f149
Yinghai Lu <yinghai@kernel.org>
2013-10-04 21:35:15 -0700

    iommu, irq: Allocate irq_desc for dmar_msi with local node
    iommu irq's irq_desc should be on local node ram.
    
    Fix the return value checking problem.
      create_irq() will return -1 when fail to allocate.
      create_irq_nr() will return 0 when fail to allocate.
    here only check !irq, so need to change it to use create_irq_nr instead.
    
    -v2: According to Sebastian, add cc to stable.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Joerg Roedel <joro@8bytes.org>
    Cc: Donald Dutile <ddutile@redhat.com>
    Acked-by: Donald Dutile <ddutile@redhat.com>
    Cc: Sebastian Andrzej Siewior <sebastian@breakpoint.cc>
    Cc: stable@vger.kernel.org

--- drivers/iommu/dmar.c
+++ drivers/iommu/dmar.c
@@ -1277,7 +1277,7 @@ int dmar_set_interrupt(struct intel_iommu *iommu)
 	if (iommu->irq)
 		return 0;
 
-	irq = create_irq();
+	irq = create_irq_nr(0, iommu->node);
 	if (!irq) {
 		pr_err("IOMMU: no free vectors\n");
 		return -EINVAL;
