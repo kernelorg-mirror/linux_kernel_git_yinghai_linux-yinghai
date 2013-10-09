61756eabffa0a530c53bf38237776ea5d5cc22e4
Yinghai Lu <yinghai@kernel.org>
2013-10-09 12:05:19 -0700

    x86, ACPI, mm: Kill max_low_pfn_mapped
    Before recent pfn_mapped array change, we assume that range
    [0, max_low_pfn_mapped) is all mapped.
    Now we only map the  RAM or KERNL_RESERVED, to prevent user from
    taking it granted that only check if addr is below max_low_pfn_mapped
    and assume it mapped, we need to kill max_low_pfn_mapped.
    
    User should use pfn_mapped[] or just 1UL<<(32-PAGE_SHIFT) instead.
    
    Only user is ACPI_INITRD_TABLE_OVERRIDE, and it should not use that,
    as later accesses is using early_ioremap(). We could change to use
    1UL<<(32-PAGE_SHIFT) with it, aka under 4G.
    
    -v2: Leave alone max_low_pfn_mapped in i915 code according to tj.
    
    Suggested-by: H. Peter Anvin <hpa@zytor.com>
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: "Rafael J. Wysocki" <rjw@sisk.pl>
    Cc: Jacob Shin <jacob.shin@amd.com>
    Cc: Pekka Enberg <penberg@kernel.org>
    Cc: linux-acpi@vger.kernel.org
    Tested-by: Thomas Renninger <trenn@suse.de>
    Reviewed-by: Tang Chen <tangchen@cn.fujitsu.com>
    Tested-by: Tang Chen <tangchen@cn.fujitsu.com>
    Acked-by: Tejun Heo <tj@kernel.org>
    Acked-by: Toshi Kani <toshi.kani@hp.com>

--- arch/x86/mm/init.c
+++ arch/x86/mm/init.c
@@ -313,10 +313,6 @@ static void add_pfn_range_mapped(unsigned long start_pfn, unsigned long end_pfn)
 	nr_pfn_mapped = clean_sort_range(pfn_mapped, E820_X_MAX);
 
 	max_pfn_mapped = max(max_pfn_mapped, end_pfn);
-
-	if (start_pfn < (1UL<<(32-PAGE_SHIFT)))
-		max_low_pfn_mapped = max(max_low_pfn_mapped,
-					 min(end_pfn, 1UL<<(32-PAGE_SHIFT)));
 }
 
 bool pfn_range_is_mapped(unsigned long start_pfn, unsigned long end_pfn)
