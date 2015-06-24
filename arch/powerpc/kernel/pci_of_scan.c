6a796dc54d83a1eaa1dd9a81e47bc5dcade6ad00
Yinghai Lu <yinghai@kernel.org>
2015-06-23 17:41:50 -0700

    powerpc/PCI: Add IORESOURCE_MEM_64 for 64-bit resource in of parsing
    For device resource PREF bit setting under bridge 64-bit pref resource,
    we need to make sure only set PREF for 64bit resource, so set
    IORESOUCE_MEM_64 for 64bit resource during of device resource flags
    parsing.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=96261
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=96241
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: Benjamin Herrenschmidt <benh@kernel.crashing.org>
    Cc: Paul Mackerras <paulus@samba.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Gavin Shan <gwshan@linux.vnet.ibm.com>
    Cc: Yijing Wang <wangyijing@huawei.com>
    Cc: Anton Blanchard <anton@samba.org>
    Cc: linuxppc-dev@lists.ozlabs.org

--- arch/powerpc/kernel/pci_of_scan.c
+++ arch/powerpc/kernel/pci_of_scan.c
@@ -44,8 +44,10 @@ static unsigned int pci_parse_of_flags(u32 addr0, int bridge)
 
 	if (addr0 & 0x02000000) {
 		flags = IORESOURCE_MEM | PCI_BASE_ADDRESS_SPACE_MEMORY;
-		flags |= (addr0 >> 22) & PCI_BASE_ADDRESS_MEM_TYPE_64;
 		flags |= (addr0 >> 28) & PCI_BASE_ADDRESS_MEM_TYPE_1M;
+		if (addr0 & 0x01000000)
+			flags |= IORESOURCE_MEM_64
+				 | PCI_BASE_ADDRESS_MEM_TYPE_64;
 		if (addr0 & 0x40000000)
 			flags |= IORESOURCE_PREFETCH
 				 | PCI_BASE_ADDRESS_MEM_PREFETCH;
