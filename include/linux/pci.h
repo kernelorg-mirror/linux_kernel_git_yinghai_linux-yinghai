18a3a630d6196d0d20f63cb7d258feae144842bc
Yinghai Lu <yinghai@kernel.org>
2015-06-23 17:41:53 -0700

    PCI: Keep more big bus range for sriov
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -816,6 +816,7 @@ void pci_stop_and_remove_bus_device(struct pci_dev *dev);
 void pci_stop_and_remove_bus_device_locked(struct pci_dev *dev);
 void pci_stop_root_bus(struct pci_bus *bus);
 void pci_remove_root_bus(struct pci_bus *bus);
+void pci_stop_and_remove_pci_bus(struct pci_bus *bus);
 void pci_setup_cardbus(struct pci_bus *bus);
 void pci_sort_breadthfirst(void);
 #define dev_is_pci(d) ((d)->bus == &pci_bus_type)
