407029cc82f4a0bff2eae07c65309a2ebdc04a08
Yinghai Lu <yinghai@kernel.org>
2015-07-05 23:59:41 -0700

    PCI: Don't release fixed resource for realloc
    We should not release bridge resource if there is fixed resources
    under it, otherwise the children firmware would stop working.
    
    Reported-by: Paul Johnson <pjay@nwtrail.com>
    Suggested-by: Bjorn Helgaas <bhelgaas@google.com>
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=92351
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Cc: stable@vger.kernel.org

--- include/linux/ioport.h
+++ include/linux/ioport.h
@@ -141,7 +141,7 @@ extern struct resource iomem_resource;
 extern struct resource *request_resource_conflict(struct resource *root, struct resource *new);
 extern int request_resource(struct resource *root, struct resource *new);
 extern int release_resource(struct resource *new);
-void release_child_resources(struct resource *new);
+bool release_child_resources(struct resource *new);
 extern void reserve_region_with_split(struct resource *root,
 			     resource_size_t start, resource_size_t end,
 			     const char *name);
