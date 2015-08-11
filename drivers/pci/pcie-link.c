61f46e1c3e111244520791af17343288ffd1874d
Yinghai Lu <yinghai@kernel.org>
2015-08-11 11:17:43 -0700

    PCI: seperate pcie_link_retrain code to another file
    Will use it from /sys .../pcie/link_retrain
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/pcie-link.c
+++ drivers/pci/pcie-link.c
@@ -40,3 +40,36 @@ void pcie_link_disable_set(struct pci_dev *dev, int bit)
 			 lnk_ctrl);
 }
 EXPORT_SYMBOL(pcie_link_disable_set);
+
+#define LINK_RETRAIN_TIMEOUT HZ
+
+int pcie_link_retrain(struct pci_dev *parent)
+{
+	u16 reg16;
+	unsigned long start_jiffies;
+
+	if (!pci_is_pcie(parent))
+		return 0;
+
+	pcie_capability_read_word(parent, PCI_EXP_LNKCTL, &reg16);
+	/* Retrain link */
+	reg16 |= PCI_EXP_LNKCTL_RL;
+	pcie_capability_write_word(parent, PCI_EXP_LNKCTL, reg16);
+	dev_printk(KERN_DEBUG, &parent->dev, "%s: lnk_ctrl = %x\n", __func__,
+			 reg16);
+
+	/* Wait for link training end. Break out after waiting for timeout */
+	start_jiffies = jiffies;
+	for (;;) {
+		pcie_capability_read_word(parent, PCI_EXP_LNKSTA, &reg16);
+		if (!(reg16 & PCI_EXP_LNKSTA_LT))
+			break;
+		if (time_after(jiffies, start_jiffies + LINK_RETRAIN_TIMEOUT))
+			break;
+		msleep(1);
+	}
+	if (!(reg16 & PCI_EXP_LNKSTA_LT))
+		return 0;
+
+	return -1;
+}
