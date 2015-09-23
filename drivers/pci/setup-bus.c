b61382de09f6feba46b21fe60fb5f0ab0958f018
Yinghai Lu <yinghai@kernel.org>
2015-09-22 19:53:34 -0700

    PCI: Get new realloc size for bridge for last try
    Current realloc path would not shrink bridge resource through
    pbus_size_mem() checking with the old size.
    
    That cause problem: when "required+optional" resource allocation fails,
    the cached bridge resource size will prevent "required" resource to get
    allocated smaller resource.
    
    Clear the old resource size for last try or third and later try.
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=81431
    Tested-by: TJ <linux@iam.tj>
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>

--- drivers/pci/setup-bus.c
+++ drivers/pci/setup-bus.c
@@ -2225,6 +2225,17 @@ static enum enable_type pci_realloc_detect(struct pci_bus *bus,
 }
 #endif
 
+static void reset_bridge_resource_size(struct pci_dev *dev,
+				       struct resource *res)
+{
+	int idx = res - &dev->resource[0];
+
+	if (idx >= PCI_BRIDGE_RESOURCES && idx <= PCI_BRIDGE_RESOURCE_END) {
+		res->start = 0;
+		res->end = res->start - 1;
+	}
+}
+
 /*
  * first try will not touch pci bridge res
  * second and later try will clear small leaf bridge res
@@ -2307,8 +2318,13 @@ again:
 		struct resource *res = fail_res->res;
 
 		restore_resource(fail_res, res);
-		if (fail_res->dev->subordinate)
+		if (fail_res->dev->subordinate) {
 			res->flags = 0;
+			/* last or third times and later */
+			if (tried_times + 1 == pci_try_num ||
+			    tried_times + 1 > 2)
+				reset_bridge_resource_size(fail_res->dev, res);
+		}
 	}
 	free_list(&fail_head);
 
@@ -2380,8 +2396,11 @@ again:
 		struct resource *res = fail_res->res;
 
 		restore_resource(fail_res, res);
-		if (fail_res->dev->subordinate)
+		if (fail_res->dev->subordinate) {
 			res->flags = 0;
+			/* last time */
+			reset_bridge_resource_size(fail_res->dev, res);
+		}
 	}
 	free_list(&fail_head);
 
