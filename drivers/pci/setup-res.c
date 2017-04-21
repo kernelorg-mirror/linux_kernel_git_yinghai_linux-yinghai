1dbb1bd6bbc20bf095811d340b739adc3f7f9c28
Yinghai Lu <yinghai@kernel.org>
2017-04-20 21:38:32 -0700

    PCI: Restore pref MMIO allocation logic for host bridge without mmio64
    From 5b2854155 (PCI: Restrict 64-bit prefetchable bridge windows to 64-bit
    resources), we change the logic for pref mmio allocation:
    When bridge pref support mmio64, we will only put children pref
    that support mmio64 into it, and will put children pref mmio32
    into bridge's non-pref mmio32.
    
    That could leave bridge pref bar not used when that pref bar is mmio64,
    and children res only has mmio32.
    Also could have allocation failure when non-pref mmio32 is not big
    enough space for those children pref mmio32.
    
    That is not rational when the host bridge does not have 64bit mmio
    above 4g at all.
    
    The patch restore to old logic:
    when host bridge does not have has_mem64, put children pref mmio64 and
    pref mmio32 all under bridges pref bars.
    
    Signed-off-by: Yinghai Lu <yinghai@kernel.org>
    Tested-by: Khalid Aziz <khalid.aziz@oracle.com>

--- drivers/pci/setup-res.c
+++ drivers/pci/setup-res.c
@@ -240,6 +240,8 @@ static int __pci_assign_resource(struct pci_bus *bus, struct pci_dev *dev,
 	struct resource *res = dev->resource + resno;
 	resource_size_t min;
 	int ret;
+	unsigned long mmio64 = pci_find_host_bridge(bus)->has_mem64 ?
+				IORESOURCE_MEM_64 : 0;
 
 	min = (res->flags & IORESOURCE_IO) ? PCIBIOS_MIN_IO : PCIBIOS_MIN_MEM;
 
@@ -251,7 +253,7 @@ static int __pci_assign_resource(struct pci_bus *bus, struct pci_dev *dev,
 	 * things differently than they were sized, not everything will fit.
 	 */
 	ret = pci_bus_alloc_resource(bus, res, size, align, min,
-				     IORESOURCE_PREFETCH | IORESOURCE_MEM_64,
+				     IORESOURCE_PREFETCH | mmio64,
 				     pcibios_align_resource, dev);
 	if (ret == 0)
 		return 0;
@@ -260,7 +262,8 @@ static int __pci_assign_resource(struct pci_bus *bus, struct pci_dev *dev,
 	 * If the prefetchable window is only 32 bits wide, we can put
 	 * 64-bit prefetchable resources in it.
 	 */
-	if ((res->flags & (IORESOURCE_PREFETCH | IORESOURCE_MEM_64)) ==
+	if (mmio64 &&
+	    (res->flags & (IORESOURCE_PREFETCH | IORESOURCE_MEM_64)) ==
 	     (IORESOURCE_PREFETCH | IORESOURCE_MEM_64)) {
 		ret = pci_bus_alloc_resource(bus, res, size, align, min,
 					     IORESOURCE_PREFETCH,
@@ -275,7 +278,7 @@ static int __pci_assign_resource(struct pci_bus *bus, struct pci_dev *dev,
 	 * non-prefetchable, the first call already tried the only possibility
 	 * so we don't need to try again.
 	 */
-	if (res->flags & (IORESOURCE_PREFETCH | IORESOURCE_MEM_64))
+	if (res->flags & (IORESOURCE_PREFETCH | mmio64))
 		ret = pci_bus_alloc_resource(bus, res, size, align, min, 0,
 					     pcibios_align_resource, dev);
 
